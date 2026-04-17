6c50ad1b0ecf3030cea5983a2a1524e464c071e0
Eloise-Code-Hub <276613342+Eloise-Code-Hub@users.noreply.github.com>
2026-04-17 20:14:38 +0800

    fix port for railway

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/client-urls/:client_slug", async (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
